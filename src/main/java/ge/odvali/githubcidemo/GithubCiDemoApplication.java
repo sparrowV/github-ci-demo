0388e610ae9d1f1ece1e74cab87b54852f30f488
sparrowV <otto.dvali@gmail.com>
2021-02-20 16:42:52 +0400

    init

---
+++ src/main/java/ge/odvali/githubcidemo/GithubCiDemoApplication.java
@@ -0,0 +1,22 @@
+package ge.odvali.githubcidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCiDemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+
+}
---
+++ src/test/java/ge/odvali/githubcidemo/EndpointIT.java
@@ -0,0 +1,15 @@
+package ge.odvali.githubcidemo;
+
+import org.jsmart.zerocode.core.domain.Scenario;
+import org.jsmart.zerocode.core.domain.TargetEnv;
+import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ZeroCodeUnitRunner.class)
+public class EndpointIT {
+
+		@Test
+		@Scenario("endpoint_scenario.json")
+		public void endpoint_test(){}
+}
